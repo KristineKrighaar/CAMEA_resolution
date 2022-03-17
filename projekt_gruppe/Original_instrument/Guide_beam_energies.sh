857ea1c562dfe84b81b0cddc0aaf9b8b34ed7040
KristineKrighaar <kristinekrighaar@hotmail.com>
2022-03-17 23:06:53 +0100

    ny data

---
+++ projekt_gruppe/Original_instrument/Guide_beam_energies.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+mcrun FullInstrument_v5.instr -d /home/kristine/Documents/GitHub/CAMEA_resolution/projekt_gruppe/Original_instrument/v5_4meV_dE1meV -n 1000000 -N 141  SourceE=4  DeltaSourceE=1  guideMonitors=1  EI=5  A3=0  A4=-45  SAMPLE=0  RV_mono_Bool=1  RH_mono_Bool=1  monoShieldingWidth=0.062  monoShieldingHeight=0.14  monoShieldingDistance=0.235  MonoShielding=1  Be_filter=1  sampleHeight=0.01  sampleRadius=0.005  samplePosY=0  simon=0,1.40
+
+mcrun FullInstrument_v5.instr -d /home/kristine/Documents/GitHub/CAMEA_resolution/projekt_gruppe/Original_instrument/v5_5meV_dE1meV -n 1000000 -N 141  SourceE=5  DeltaSourceE=1  guideMonitors=1  EI=5  A3=0  A4=-45  SAMPLE=0  RV_mono_Bool=1  RH_mono_Bool=1  monoShieldingWidth=0.062  monoShieldingHeight=0.14  monoShieldingDistance=0.235  MonoShielding=1  Be_filter=1  sampleHeight=0.01  sampleRadius=0.005  samplePosY=0  simon=0,1.40
+
+
+mcrun FullInstrument_v5.instr -d /home/kristine/Documents/GitHub/CAMEA_resolution/projekt_gruppe/Original_instrument/v5_6meV_dE1meV -n 1000000 -N 141  SourceE=6  DeltaSourceE=1  guideMonitors=1  EI=5  A3=0  A4=-45  SAMPLE=0  RV_mono_Bool=1  RH_mono_Bool=1  monoShieldingWidth=0.062  monoShieldingHeight=0.14  monoShieldingDistance=0.235  MonoShielding=1  Be_filter=1  sampleHeight=0.01  sampleRadius=0.005  samplePosY=0  simon=0,1.40
+
+mcrun FullInstrument_v5.instr -d /home/kristine/Documents/GitHub/CAMEA_resolution/projekt_gruppe/Original_instrument/v5_7meV_dE1meV -n 1000000 -N 141  SourceE=7  DeltaSourceE=1  guideMonitors=1  EI=5  A3=0  A4=-45  SAMPLE=0  RV_mono_Bool=1  RH_mono_Bool=1  monoShieldingWidth=0.062  monoShieldingHeight=0.14  monoShieldingDistance=0.235  MonoShielding=1  Be_filter=1  sampleHeight=0.01  sampleRadius=0.005  samplePosY=0  simon=0,1.40
+
+
+mcrun FullInstrument_v5.instr -d /home/kristine/Documents/GitHub/CAMEA_resolution/projekt_gruppe/Original_instrument/v5_8meV_dE1meV -n 1000000 -N 141  SourceE=8  DeltaSourceE=1  guideMonitors=1  EI=5  A3=0  A4=-45  SAMPLE=0  RV_mono_Bool=1  RH_mono_Bool=1  monoShieldingWidth=0.062  monoShieldingHeight=0.14  monoShieldingDistance=0.235  MonoShielding=1  Be_filter=1  sampleHeight=0.01  sampleRadius=0.005  samplePosY=0  simon=0,1.40
+
+
+mcrun FullInstrument_v5.instr -d /home/kristine/Documents/GitHub/CAMEA_resolution/projekt_gruppe/Original_instrument/v5_9meV_dE1meV -n 1000000 -N 141  SourceE=9  DeltaSourceE=1  guideMonitors=1  EI=5  A3=0  A4=-45  SAMPLE=0  RV_mono_Bool=1  RH_mono_Bool=1  monoShieldingWidth=0.062  monoShieldingHeight=0.14  monoShieldingDistance=0.235  MonoShielding=1  Be_filter=1  sampleHeight=0.01  sampleRadius=0.005  samplePosY=0  simon=0,1.40
+
+
+mcrun FullInstrument_v5.instr -d /home/kristine/Documents/GitHub/CAMEA_resolution/projekt_gruppe/Original_instrument/v5_10meV_dE1meV -n 1000000 -N 141  SourceE=10  DeltaSourceE=1  guideMonitors=1  EI=5  A3=0  A4=-45  SAMPLE=0  RV_mono_Bool=1  RH_mono_Bool=1  monoShieldingWidth=0.062  monoShieldingHeight=0.14  monoShieldingDistance=0.235  MonoShielding=1  Be_filter=1  sampleHeight=0.01  sampleRadius=0.005  samplePosY=0  simon=0,1.40
+
+
+mcrun FullInstrument_v5.instr -d /home/kristine/Documents/GitHub/CAMEA_resolution/projekt_gruppe/Original_instrument/v5_11meV_dE1meV -n 1000000 -N 141  SourceE=11  DeltaSourceE=1  guideMonitors=1  EI=5  A3=0  A4=-45  SAMPLE=0  RV_mono_Bool=1  RH_mono_Bool=1  monoShieldingWidth=0.062  monoShieldingHeight=0.14  monoShieldingDistance=0.235  MonoShielding=1  Be_filter=1  sampleHeight=0.01  sampleRadius=0.005  samplePosY=0  simon=0,1.40
+
+
+mcrun FullInstrument_v5.instr -d /home/kristine/Documents/GitHub/CAMEA_resolution/projekt_gruppe/Original_instrument/v5_12meV_dE1meV -n 1000000 -N 141  SourceE=12  DeltaSourceE=1  guideMonitors=1  EI=5  A3=0  A4=-45  SAMPLE=0  RV_mono_Bool=1  RH_mono_Bool=1  monoShieldingWidth=0.062  monoShieldingHeight=0.14  monoShieldingDistance=0.235  MonoShielding=1  Be_filter=1  sampleHeight=0.01  sampleRadius=0.005  samplePosY=0  simon=0,1.40
+
+
+mcrun FullInstrument_v5.instr -d /home/kristine/Documents/GitHub/CAMEA_resolution/projekt_gruppe/Original_instrument/v5_13meV_dE1meV -n 1000000 -N 141  SourceE=13  DeltaSourceE=1  guideMonitors=1  EI=5  A3=0  A4=-45  SAMPLE=0  RV_mono_Bool=1  RH_mono_Bool=1  monoShieldingWidth=0.062  monoShieldingHeight=0.14  monoShieldingDistance=0.235  MonoShielding=1  Be_filter=1  sampleHeight=0.01  sampleRadius=0.005  samplePosY=0  simon=0,1.40
+
+
+
+mcrun FullInstrument_v5.instr -d /home/kristine/Documents/GitHub/CAMEA_resolution/projekt_gruppe/Original_instrument/v5_14meV_dE1meV -n 1000000 -N 141  SourceE=14  DeltaSourceE=1  guideMonitors=1  EI=5  A3=0  A4=-45  SAMPLE=0  RV_mono_Bool=1  RH_mono_Bool=1  monoShieldingWidth=0.062  monoShieldingHeight=0.14  monoShieldingDistance=0.235  MonoShielding=1  Be_filter=1  sampleHeight=0.01  sampleRadius=0.005  samplePosY=0  simon=0,1.40
+
+
+mcrun FullInstrument_v5.instr -d /home/kristine/Documents/GitHub/CAMEA_resolution/projekt_gruppe/Original_instrument/v5_15meV_dE1meV -n 1000000 -N 141  SourceE=15  DeltaSourceE=1  guideMonitors=1  EI=5  A3=0  A4=-45  SAMPLE=0  RV_mono_Bool=1  RH_mono_Bool=1  monoShieldingWidth=0.062  monoShieldingHeight=0.14  monoShieldingDistance=0.235  MonoShielding=1  Be_filter=1  sampleHeight=0.01  sampleRadius=0.005  samplePosY=0  simon=0,1.40
+    
+    
+    
+   
+
